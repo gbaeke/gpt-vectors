45136d545fcf7e251b8a3041c4a62c0f7dfb72fe
gbaeke <geert@baeke.info>
2023-03-19 14:54:13 +0100

    model selection and check

--- webapp/app.py
+++ webapp/app.py
@@ -1,3 +1,4 @@
+from distutils.log import debug
 import os
 import logging
 from re import M
@@ -10,6 +11,7 @@ import tiktoken
 
 app = Flask(__name__)
 
+## returns url and score if score is high enough
 def get_highest_score_url(items):
     highest_score_item = max(items, key=lambda item: item["score"])
 
@@ -18,6 +20,7 @@ def get_highest_score_url(items):
     
     return "", 0
 
+# init pinecone and openai
 pinecone_api = os.getenv('PINECONE_API_KEY')
 pinecone_env = os.getenv('PINECONE_ENVIRONMENT')
 
@@ -27,28 +30,38 @@ index = pinecone.Index('blog-index')
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+# default page
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# respond to submit button
 @app.route('/query', methods=['POST'])
 def query():
-    your_query = request.form.get('query')
-    model = request.form.get('model')
-
-    # set max tokens based on model
-    max_tokens = 250
-    if model == "gpt-4":
-        max_tokens = 1024
-
     # if query is empty, return empty response
+    your_query = request.form.get('query')
     if your_query == "":
         return jsonify({
             'url': "",
             'score': 0,
             'response': "Please specify a query!"
         })
+    
+    # get model from form and check if allowed
+    model = request.form.get('model')
+    if model not in allowed_models:
+        return jsonify({
+            'url': "",
+            'score': 0,
+            'response': "Invalid model. Please try again."
+        })    
 
+    # default max tokens for reply is higher for gpt-4
+    max_tokens = 250
+    if model == "gpt-4":
+        max_tokens = 1024
+
+    # vectorize query
     try:
         query_vector = openai.Embedding.create(
             input=your_query,
@@ -57,16 +70,21 @@ def query():
     except Exception as e:
         logging.error("Error calling OpenAI Embedding API: ", exc_info=True)
 
+    # query with pinecone
     search_response = []
     search_response = index.query(
         top_k=5,
         vector=query_vector,
         include_metadata=True)
     
-    print(search_response)
+    # log search response for debugging
+    logging.debug("Search response: %s", search_response)
 
+    # get url with highest score, might return empty string if score is too low
+    # also returns score, score is 0 if url is empty
     url, score = get_highest_score_url(search_response['matches'])
 
+    # if url is empty, return empty response
     if url == "":
         return jsonify({
             'url': "",
@@ -77,9 +95,11 @@ def query():
     logging.debug("Highest score url: %s", url)
 
     try:
+        # get article from url and parse HTML
         r = requests.get(url)
         soup = BeautifulSoup(r.text, 'html.parser')
 
+        # do not grab the entire HTML
         article = soup.find('div', {'class': 'entry-content'}).text
     except Exception as e:
         logging.error("Error getting article: ", exc_info=True)
@@ -90,6 +110,8 @@ def query():
         })
 
     try:
+        # call openai completion, model is set by user
+        # ensure openai key allows gpt-4 use
         response = openai.ChatCompletion.create(
             model=model,
             messages=[
@@ -100,6 +122,7 @@ def query():
             ],
             temperature=0,
             max_tokens=max_tokens
+
         )
 
         response_text=f"\n{response.choices[0]['message']['content']}"
@@ -120,5 +143,7 @@ def query():
 if __name__ == '__main__':
     log_level = os.getenv('LOG_LEVEL', 'ERROR').upper()
     logging.basicConfig(level=getattr(logging, log_level))
+
+    allowed_models = ["gpt-3.5-turbo", "gpt-4"]
 
     app.run(debug=True)
